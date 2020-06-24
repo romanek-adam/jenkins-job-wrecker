00838e5df7f243a070dd9450ee333c447f44e4fe
Eren ATAS <eatas.contractor@libertyglobal.com>
2020-06-24 17:34:17 +0200

    Implemented missing tags for GHPRB plugin
    Implementation is currently in sync with JJB. Added a test case in order to compare result.
    
    Signed-off-by: Eren ATAS <eatas.contractor@libertyglobal.com>

--- tests/test_jjb.py
+++ tests/test_jjb.py
@@ -3,6 +3,7 @@ from jenkins_job_wrecker.cli import get_xml_root, root_to_yaml, setup_str_presen
 import os
 import tempfile
 from jenkins_jobs.cli.entry import JenkinsJobs
+from .helpers import compare_jjb_output
 
 fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 
@@ -24,17 +25,6 @@ class TestJJB(object):
             jjb = JenkinsJobs(['test', temp.name])
             assert jjb.execute() is None
 
-    def compare_jjb_output(self, name, job_name):
-        test_filename = os.path.join(fixtures_path, name + '.xml')
-        expected_yml_filename = os.path.join(fixtures_path, name+'.yml')
-        root = get_xml_root(filename=test_filename)
-        actual_yml = root_to_yaml(root, job_name)
-
-        with open(expected_yml_filename) as f:
-            expected_yml = f.read()
-
-        assert actual_yml is not None
-        assert actual_yml == expected_yml
 
     def test_ice_setup(self):
         self.run_jjb('ice-setup')
@@ -47,9 +37,8 @@ class TestJJB(object):
 
     def test_indentation_with_tabs(self):
         setup_str_presenter(True)
-        self.compare_jjb_output('indentation_with_tab', 'indentation_with_tab')
-
+        compare_jjb_output(fixtures_path, 'indentation_with_tab', 'indentation_with_tab')
 
     def test_indentation_without_tabs(self):
         setup_str_presenter(False)
-        self.compare_jjb_output('indentation_without_tab', 'indentation_without_tab')
+        compare_jjb_output(fixtures_path, 'indentation_without_tab', 'indentation_without_tab')
